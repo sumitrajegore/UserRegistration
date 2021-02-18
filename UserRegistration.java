bafdef2eee06f6fc83e1a2410ee3705b5275799f
sumitrajegore <sumitrajegorevp@gmail.com>
2021-02-19 00:48:26 +0530

    [sumit]: valid passward rule at least 1 capital char and numeriic,Special char

--- UserRegistration.java
+++ UserRegistration.java
@@ -10,12 +10,12 @@ public class UserRegistration {
 		String name;
 		System.out.println("enter passward :");
 		name = sc.nextLine();
-		passward2(name);
+		passward3(name);
 	}
-	public static void passward2(String name) {
+	public static void passward3(String name) {
 
                 //Pattern p = Pattern.compile("^[a-zA-Z0-9]{3}+(.{1}[a-zA-Z0-9]{3}+)*@{1}[a-zA-Z0-9]{2}+(.{1}[a-zA-Z0-9]{2}+)*(.{1}[a-zA-Z0-9]{2}+)$");
-		Pattern p = Pattern.compile("^([A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}$");
+		Pattern p = Pattern.compile("^[\\@\\&\\$\\#\\_\\%\\,\\.](?=.*[A-Z])(?=.*[0-9]).{8,}$");
                 Matcher m = p.matcher(name);
 		if(m.find())
 		{
