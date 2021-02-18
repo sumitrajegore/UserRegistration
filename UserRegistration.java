8913ea03888f05e1f7a50e2b49d873e1b202cf77
sumitrajegore <sumitrajegorevp@gmail.com>
2021-02-18 23:39:58 +0530

    [sumit]: valid passward rule 1

--- UserRegistration.java
+++ UserRegistration.java
@@ -8,21 +8,22 @@ public class UserRegistration {
 
 		Scanner sc = new Scanner(System.in);
 		String name;
-		System.out.println("enter email id :");
+		System.out.println("enter passward :");
 		name = sc.nextLine();
-		email(name);
+		passward(name);
 	}
-	public static void email(String name) {
+	public static void passward(String name) {
 
-                Pattern p = Pattern.compile("^[a-zA-Z0-9]{3}+(.{1}[a-zA-Z0-9]{3}+)*@{1}[a-zA-Z0-9]{2}+(.{1}[a-zA-Z0-9]{2}+)*(.{1}[a-zA-Z0-9]{2}+)$");
-		Matcher m = p.matcher(name);
+                //Pattern p = Pattern.compile("^[a-zA-Z0-9]{3}+(.{1}[a-zA-Z0-9]{3}+)*@{1}[a-zA-Z0-9]{2}+(.{1}[a-zA-Z0-9]{2}+)*(.{1}[a-zA-Z0-9]{2}+)$");
+		Pattern p = Pattern.complie("^[a-zA-Z0-9]{8,}$");
+                Matcher m = p.matcher(name);
 		if(m.find())
 		{
-			System.out.println("email-id is valid");
+			System.out.println("passward is valid");
 		}
 		else
 		{
-			System.out.println("email-id is invalid");
+			System.out.println("passward is invalid");
 		}
 		System.out.println("length :"+name.length());
 	}
