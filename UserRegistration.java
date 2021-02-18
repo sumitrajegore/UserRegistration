7a9a166f3d6148e3db1c126ef2f82746e734d7b5
sumitrajegore <sumitrajegorevp@gmail.com>
2021-02-19 01:23:35 +0530

    [sumit]: check all email samples

--- UserRegistration.java
+++ UserRegistration.java
@@ -8,22 +8,22 @@ public class UserRegistration {
 
 		Scanner sc = new Scanner(System.in);
 		String name;
-		System.out.println("enter passward :");
+		System.out.println("enter email :");
 		name = sc.nextLine();
-		passward3(name);
+		allemail(name);
 	}
-	public static void passward3(String name) {
+	public static void allemail(String name) {
 
                 //Pattern p = Pattern.compile("^[a-zA-Z0-9]{3}+(.{1}[a-zA-Z0-9]{3}+)*@{1}[a-zA-Z0-9]{2}+(.{1}[a-zA-Z0-9]{2}+)*(.{1}[a-zA-Z0-9]{2}+)$");
-		Pattern p = Pattern.compile("^[\\@\\&\\$\\#\\_\\%\\,\\.](?=.*[A-Z])(?=.*[0-9]).{8,}$");
+		Pattern p = Pattern.compile("^[a-z]{3,}([\\.\\+\\-]?[0-9]{3,})?[@][a-z0-9]{1,}[.][a-z]{2,4}[,]?([.][a-z]{2,4}[.]?)?$");
                 Matcher m = p.matcher(name);
 		if(m.find())
 		{
-			System.out.println("passward is valid");
+			System.out.println("email is valid");
 		}
 		else
 		{
-			System.out.println("passward is invalid");
+			System.out.println("email is invalid");
 		}
 		System.out.println("length :"+name.length());
 	}
