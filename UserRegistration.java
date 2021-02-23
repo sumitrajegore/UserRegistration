76e83c8573a5eac828871cb1cabdcec73a83890b
sumitrajegore <sumitrajegorevp@gmail.com>
2021-02-24 04:54:05 +0530

    Added regex concepts using custum expection

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,29 +1,69 @@
-import java.util.Scanner;
+package com.userregistration;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(System.in);
-		String name;
-		System.out.println("enter last name :");
-		name = sc.nextLine();
-		lastname(name);
-	}
-	public static void lastname(String name) {
-		
-		Pattern p = Pattern.compile("^[A-Z]{1}[A-Za-z]{2,}+$");
-		Matcher m = p.matcher(name);
-		if(m.find())
-		{
-			System.out.println("lastname is valid");
-		}
-		else
-		{
-			System.out.println("lastname is invalid");
-		}
-		System.out.println("length :"+name.length());
-	}
+    public boolean nameValid(String name) throws UserRegistrationTestException {
+       try {
+    	   if (name.isEmpty()) throw new UserRegistrationTestException("Entered value is empty", UserRegistrationTestException.ExceptionType.EmptyType);
+    	   
+ 	       	Pattern p = Pattern.compile("^[A-Z][a-zA-Z]{3,}");
+        	Matcher m = p.matcher(name);
+        	return m.find(); 
+       } catch(NullPointerException e) {
+           throw new UserRegistrationTestException("entered null", UserRegistrationTestException.ExceptionType.NullType);
+  
+       }
+    }
+
+    public boolean lastValid(String name) throws UserRegistrationTestException {
+        try {
+     	   if (name.isEmpty()) throw new UserRegistrationTestException("Entered value is empty", UserRegistrationTestException.ExceptionType.EmptyType);
+
+        	Pattern p = Pattern.compile("^[A-Z][a-zA-Z]{3,}");
+        	Matcher m = p.matcher(name);
+        	return m.find();
+        } catch(NullPointerException e) { 
+            throw new UserRegistrationTestException("entered null", UserRegistrationTestException.ExceptionType.NullType); 
+            }
+    }
+    
+    public boolean emailValid(String email) throws UserRegistrationTestException {
+    	try { 
+	    if (email.isEmpty()) throw new UserRegistrationTestException("Entered value is empty", UserRegistrationTestException.ExceptionType.EmptyType);
+
+        	Pattern p = Pattern.compile("^abc[a-zA-Z0-9.]*@bl[.]co[.][a-z]{2,3}");
+        	Matcher m = p.matcher(email);
+        	return m.find();
+    	} catch(NullPointerException e) {
+            throw new UserRegistrationTestException("entered null", UserRegistrationTestException.ExceptionType.NullType);
+    	}
+    }
+
+    public boolean phoneValid(String phoneNumber) throws UserRegistrationTestException {
+       try {
+    	   if (phoneNumber.isEmpty()) throw new UserRegistrationTestException("Entered value is empty", UserRegistrationTestException.ExceptionType.EmptyType);
+
+        	Pattern p = Pattern.compile("^[0-9]{2}[\\s][7-9]{2}[0-9]{8}");
+        	Matcher m = p.matcher(phoneNumber);
+        	return m.find();
+       } catch(NullPointerException e) {
+           throw new UserRegistrationTestException("entered null", UserRegistrationTestException.ExceptionType.NullType);
+       }
+    }
+
+    public boolean passwordValid(String password) throws UserRegistrationTestException {
+       try { 
+ 	   if (password.isEmpty()) throw new UserRegistrationTestException("Entered value is empty", UserRegistrationTestException.ExceptionType.EmptyType);
+        
+        	Pattern p = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()]).{8,}$");
+      	  	Matcher m = p.matcher(password);
+        	return m.find();
+        } catch(NullPointerException e) {
+            throw new UserRegistrationTestException("entered null", UserRegistrationTestException.ExceptionType.NullType);
+	
+        }
+    }
+
 }
